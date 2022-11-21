c3dc628d5e0d8fc9fad3439f10c72ee3c1870404
LumbaBalumba <89866451+LumbaBalumba@users.noreply.github.com>
2022-11-21 23:24:43 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-mkdir build
-mkdir tmp
-gcc -o tmp/main.o main.c -c
-gcc -o tmp/command_exec.o command_exec.c -c
-gcc -o build/kolmlenka_shell tmp/main.o tmp/command_exec.o -D_GNU_SOURCE
-rm -rf tmp
