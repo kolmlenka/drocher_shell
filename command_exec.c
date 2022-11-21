8f224c9277341a90b325c9a744065f892aa821a1
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2022-11-21 22:40:47 +0300

    Basic loop created

--- command_exec.c
+++ command_exec.c
@@ -3,17 +3,18 @@
 #include <unistd.h>
 
 int cmd_exec(const char *cmd, char *const *args, int in_fileno,
-             int out_fileno) {
-  dup2(STDIN_FILENO, in_fileno);
-  dup2(STDOUT_FILENO, out_fileno);
-  pid_t pid = fork();
-  if (pid < 0) {
-    fprintf(stderr, "Error: could not create subprocess\n");
-    return -1;
-  } else if (!pid) {
-    execvp(cmd, args);
-    return -1;
-  } else {
-    return 0;
-  }
+             int out_fileno)
+{
+    dup2(STDIN_FILENO, in_fileno);
+    dup2(STDOUT_FILENO, out_fileno);
+    pid_t pid = fork();
+    if (pid < 0) {
+        fprintf(stderr, "Error: could not create subprocess\n");
+        return -1;
+    } else if (!pid) {
+        execvp(cmd, args);
+        return -1;
+    } else {
+        return 0;
+    }
 }
--- main.c
+++ main.c
@@ -1,7 +1,25 @@
+#include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char **argv, char **envp) {
-    printf("Hello world\n");
+const char *initstr = "[%s@%s]: ";
+
+enum
+{
+    MAX_CMD_LEN = 4096
+};
+
+int main(int argc, char **argv, char **envp)
+{
+    printf("Penis project by kolenmka (aka drocher228)\n");
+    while (true) {
+        char cur_dir[PATH_MAX + 1];
+        char cmd[MAX_CMD_LEN + 1];
+        getcwd(cur_dir, PATH_MAX);
+        printf(initstr, getenv("USERNAME"), cur_dir);
+        scanf("%s", cmd);
+        fflush(stdout);
+    }
 }
